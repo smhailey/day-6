1f15ca90c1904ccc757c9adf149b278da731da71
smhailey <smhailey@msn.com>
2019-06-10 15:23:30 -0600

    Initialized again

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+let threes = []
+let x = 0;
+do {
+  if (x % 3 === 0) {
+    threes.push(x);
+  }
+  x++;
+}
+while (x <= 100);
+
+
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     document.write("Monday");
+//     break;
+//   case 2:
+//     document.write("Tuesday");
+//     break;
+//   case 3:
+//     document.write("Wednesday");
+//     break;
+//   default:
+//     document.write("Another day");
+// }
+// Outputs "Tuesday"
+
+
+
+// function add() {
+//   console.log(1 + 2)
+// }
+// console.log(add)
+// console.log(add())
+// document.write(add)
+
+
+// console.log(`My first js app output: ${2 + 5}`)
+
+
+// let sentence1 = "hello"
+// let sentence2 = " world"
+// console.log(sentence1 + sentence2)
+
+
+// let x = 8
+// console.log(x == 9)
+// console.log('x >= 8', x >= 8)
+// console.log(x > 8 && x > 9)
+// console.log(true || true)
+// console.log(true || false)
+
+
+// sum = 10 + "5"
+// let remainder = 13 % 5
+// console.log(sum)
+// console.log(remainder)
+// console.log(remainder++)
+// console.log(remainder)
+// console.log(++remainder)
